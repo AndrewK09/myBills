ba1d1f82d17cd42b689c363e4442c245276d0015
Andrew Kan <Andrewkn09@Gmail.com>
2019-06-29 23:12:28 -0400

    Fix server to look for environment port

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const routes = require('./routes/routes.js');
 
 app.use('/', routes);
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+let port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log(`listening on port ${port}!`);
 });
